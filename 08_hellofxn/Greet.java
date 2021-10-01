1f0171d8091a54721f0b349fd24c0b09e0abcb29
May Qiu <90876910+Mq0509@users.noreply.github.com>
2021-09-30 21:25:44 -0400

    Update Greet.java

---
+++ 08_hellofxn/Greet.java
@@ -0,0 +1,7 @@
+public class Greet{
+	public static  void main (String [] args){
+		System.out.println ("Hola! How are you doing, Ducky?");
+    System.out.println ("Hello, nice to meet you, Faiyaz!");
+    System.out.println ("Hi, long time no see, Salaj.");
+	}
+}
