03f6fe40900e4eb0fe8004bd45ae9c49a7d52c55
May Qiu <90876910+Mq0509@users.noreply.github.com>
2021-10-03 19:56:29 -0400

    Update BigSib.java

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -1,7 +1,7 @@
 public class BigSib{
   public static void main (String[]arg){
   }
-  public static void greet(String[]arg){
+  public static void greet(String[]){
     System.out.println("Nice to meet you, " + hi)
   }
 }
