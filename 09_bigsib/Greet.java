f167a5e69b8a2b19d6ca1375cd0917ddad502894
May Qiu <90876910+Mq0509@users.noreply.github.com>
2021-10-03 20:28:58 -0400

    Update Greet.java

--- 09_bigsib/Greet.java
+++ 09_bigsib/Greet.java
@@ -1,3 +1,19 @@
+/*
+May Qiu, Salaj R. , Faiyaz R, Perry Jr, Atlas, Pivot, and George
+APCS
+HW09-- What aRE BigSibs Good For? /Greetings across folders /Writing a bigsib function so that the Greet works similiarily
+2021-10-03
+*/
+
+/*
+DISCOVERIES
+I discovered that functions placed in one class can be identified as the Classname.functionname. Another discovery is that functions on different files on the same folder can be used.
+
+UNRESOLVED QUESTIONS
+I do not really have questions from this homework.
+
+*/
+
 public class Greet{
   public static void main (String [] arg){
     BigSib.greet("Jasmine");
