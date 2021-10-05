11ae0ca189fff6ccb3c5ad82c413c03beedf9ca2
May Qiu <90876910+Mq0509@users.noreply.github.com>
2021-10-04 20:37:01 -0400

    Update Greet.java

--- 10_bigsib/Greet.java
+++ 10_bigsib/Greet.java
@@ -1,8 +1,7 @@
 public class Greet {
     public static String main( String[] args ){
-        return 
-        (System.out.println( BigSib.greet("George"));
+        System.out.println( BigSib.greet("George"));
         System.out.println( BigSib.greet("Atlas"));
-        System.out.println( BigSib.greet("Perry.Jr")););
+        System.out.println( BigSib.greet("Perry.Jr"));
 }
 }
