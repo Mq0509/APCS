b9815eabc0c9c43a474c04f46ea1898019734e4a
May Qiu <90876910+Mq0509@users.noreply.github.com>
2021-10-05 20:59:03 -0400

    Update BigSib.java

--- 12_bigsib/BigSib.java
+++ 12_bigsib/BigSib.java
@@ -1,8 +1,8 @@
 public class BigSib{
   public static void main (String[]arg){
+  }
     public static String (String HelloMsg){
       SetHelloMsg = HelloMsg;
-    }
   }
   public static String greet(String hi){
     return SetHelloMsg + hi;
