25e323c6fc34f759ac99530fbadd244bc833574c
May Qiu <90876910+Mq0509@users.noreply.github.com>
2021-10-05 20:38:25 -0400

    Update Greet.java

--- 12_bigsib/Greet.java
+++ 12_bigsib/Greet.java
@@ -8,5 +8,19 @@ public class Greet {
     greeting = richard.greet("freshman");
     System.out.println(greeting);
 
+    richard.setHelloMsg("Salutations");
+
+    greeting = richard.greet("Dr. Spaceman");
+    System.out.println(greeting);
+
+    richard.setHelloMsg("Hey ya");
+
+    greeting = richard.greet("Kong Fooey");
+    System.out.println(greeting);
+
+    richard.setHelloMsg("Sup");
+
+    greeting = richard.greet("mom");
+    System.out.println(greeting);
   } //end main()
-} 
+} //end Greet
