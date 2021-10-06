846e75f5665963db67b1137aee00520142021ada
May Qiu <90876910+Mq0509@users.noreply.github.com>
2021-10-05 21:31:44 -0400

    Update BigSib.java

--- 12_bigsib/BigSib.java
+++ 12_bigsib/BigSib.java
@@ -1,3 +1,14 @@
+May Qiu, Raven
+APCS
+HW12 -- On Elder Individuality and the Elimination of Radio Fuzz/Greetings with more variables/Created a new function that updated the value of 
+setHelloMsg so that the greeting is more unique for each greeting
+2021-10-05
+
+DISCO
+Since everything is local, I would need to make a function updating the variable by creating a variable outside of the methods.
+QCC
+none
+
 public class BigSib{
   public static void main (String[]arg){
   }
