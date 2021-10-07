7b20d06385bde5ca7be5a6cfc00986188e86e6c2
May Qiu <90876910+Mq0509@users.noreply.github.com>
2021-10-06 22:41:50 -0400

    Update BigSib.java

---
+++ 13_hatch/BigSib.java
@@ -0,0 +1,14 @@
+public class BigSib{
+public BigSib(){                                          /* Constructor
+  String helloMsg = "Word up";
+}
+  public static void main (String[]arg){
+  }
+  private String HelloMsg;                               /*Instance Variable
+    public static void setHelloMsg(String helloMsg){
+       HelloMsg = helloMsg;
+  }
+  public static String greet(String hi){
+    return HelloMsg + hi;
+  }
+}
