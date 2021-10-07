ca860d9376d229046aa7b3b122250d27d6e17b66
May Qiu <90876910+Mq0509@users.noreply.github.com>
2021-10-06 22:42:20 -0400

    Update Greet.java

---
+++ 13_hatch/Greet.java
@@ -0,0 +1,25 @@
+public class Greet {
+  public static void main( String[] args ) {
+    String greeting;
+
+    BigSib richard = new BigSib();
+
+    greeting = richard.greet("freshman");
+    System.out.println(greeting);
+
+    richard.setHelloMsg("Salutations ");
+
+    greeting = richard.greet("Dr. Spaceman");
+    System.out.println(greeting);
+
+    richard.setHelloMsg("Hey ya ");
+
+    greeting = richard.greet("Kong Fooey");
+    System.out.println(greeting);
+
+    richard.setHelloMsg("Sup ");
+
+    greeting = richard.greet("mom");
+    System.out.println(greeting);
+  } //end main()
+} //end Greet
