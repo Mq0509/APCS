6a0d9159fe69757666885d2f3dc46e0d043d2dfd
May Qiu <90876910+Mq0509@users.noreply.github.com>
2021-10-07 20:35:17 -0400

    Update Greet.java

---
+++ 14_overload/overcon/Greet.java
@@ -0,0 +1,15 @@
+public class Greet {
+        public static void main( String[] args ) {
+                String greeting;
+                BigSib richard = new BigSib("Word up");
+                BigSib grizz = new BigSib("Hey ya");
+                BigSib dotCom = new BigSib("Sup");
+                BigSib tracy = new BigSib("Salutations");
+                greeting = richard.greet("freshman");
+                System.out.println(greeting);
+                greeting = tracy.greet("Dr. Spaceman");
+                System.out.println(greeting);
+                greeting = dotCom.greet("mom");
+                System.out.println(greet);
+         }
+       }
