ad39360ad3770a0c0731279f4f2b6a10840242f2
May Qiu <90876910+Mq0509@users.noreply.github.com>
2021-10-07 20:35:50 -0400

    Update BigSib.java

---
+++ 14_overload/overcon/BigSib.java
@@ -0,0 +1,14 @@
+  public class BigSib{                                  
+  private String HelloMsg; /*Instance Variable*/
+  public BigSib(){              /* Constructor*/
+    HelloMsg = "Word up";
+  }
+  public static void main (String[]arg){
+  }
+    public void setHelloMsg(String helloMsg){
+       HelloMsg = helloMsg;
+  }
+  public String greet(String hi){
+    return HelloMsg + hi;
+  }
+}
