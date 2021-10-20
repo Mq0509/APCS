5a8245658fab39ccb04cf4b622df10a3d430d513
May Qiu <90876910+Mq0509@users.noreply.github.com>
2021-10-19 21:51:06 -0400

    Update Methods.java

---
+++ pre/06/Methods.java
@@ -0,0 +1,9 @@
+public class Methods {
+  public static boolean isDivisible (int n, int m) {
+     return ((n % m) == 0);
+  }
+  public static boolean isTriangle (int a, int b, int c) {
+    return ((a + b > c) && (a + c > b) && (b + c > a));
+  }
+  
+  /** I had trouble with the next part **/
