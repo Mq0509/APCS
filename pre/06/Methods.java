830c84f437a6eeda43416f0eecb98e6c0bdb3b52
mqiu30 <mqiu30@stuy.edu>
2021-10-19 22:47:36 -0400

    init setup

--- pre/06/Methods.java
+++
@@ -1,9 +0,0 @@
-public class Methods {
-  public static boolean isDivisible (int n, int m) {
-     return ((n % m) == 0);
-  }
-  public static boolean isTriangle (int a, int b, int c) {
-    return ((a + b > c) && (a + c > b) && (b + c > a));
-  }
-  
-  /** I had trouble with the next part **/
