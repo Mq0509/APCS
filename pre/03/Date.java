8afa07a379cdde661a9c5062cac2cd36813e576e
May Qiu <90876910+Mq0509@users.noreply.github.com>
2021-10-19 21:44:37 -0400

    Update Date.java

---
+++ pre/03/Date.java
@@ -0,0 +1,26 @@
+public class Date{
+  public static void main (String[] args) {
+    String day = ("Thursday");
+    int date = 26;
+    String month = ("August");
+    int year = 2021;
+    System.out.println("American format");
+    System.out.print(day);
+    System.out.print(",");
+    System.out.print(" ");
+    System.out.print(month);
+    System.out.print(" ");
+    System.out.print(date);
+    System.out.print(",");
+    System.out.print(" ");
+    System.out.println(year);
+    System.out.println("European format");
+    System.out.print(day);
+    System.out.print(" ");
+    System.out.print(date);
+    System.out.print(" ");
+    System.out.print(month);
+    System.out.print(" ");
+    System.out.print(year);
+    }
+  }
