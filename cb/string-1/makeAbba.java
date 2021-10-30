d5cc433eb0e3ed1be203f57c395c83875947532b
May Qiu <90876910+Mq0509@users.noreply.github.com>
2021-10-29 23:44:21 -0400

    Update makeAbba.java

--- cb/string-1/makeAbba.java
+++ cb/string-1/makeAbba.java
@@ -1,3 +1,12 @@
-public String makeAbba(String a, String b) {
+/* Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi". */
+
+public class makeAbba{
+  public static String makeAbba(String a, String b) {
   return a + b + b + a;
+}
+  public static String main(String [] args){
+    return makeAbba("Hi", "Bye") /* → "HiByeByeHi" */
+    return makeAbba("Yo", "Alice") /* → "YoAliceAliceYo" */
+    return makeAbba("What", "Up") /* → "WhatUpUpWhat" */
+    }
 }
