6096150d0fbaf0e73d0846f8117f7dc24d2c1b1a
May Qiu <90876910+Mq0509@users.noreply.github.com>
2021-10-30 00:04:13 -0400

    Update doubleChar.java

--- cb/string-2/doubleChar.java
+++ cb/string-2/doubleChar.java
@@ -1,9 +1,18 @@
-public String doubleChar(String str) {
+/* Given a string, return a string where for every char in the original, there are two chars. */
+
+public class doubleChar{
+  public String doubleChar(String str) {
   String ans="";
   for(int i=0;i<str.length();i++){
     ans += str.substring(i,i+1);
     ans += str.substring(i,i+1);
   }
   return ans;
+}
+  public static void main(String [] args){
+    System.out.println (doubleChar("The")) /* → "TThhee" */
+    System.out.println (doubleChar("AAbb")) /* → "AAAAbbbb" */
+    System.out.println (doubleChar("Hi-There")) /* → "HHii--TThheerree" */
+  }
 }
 
