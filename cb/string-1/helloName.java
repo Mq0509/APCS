c1972f5dbf0c9d2af7dbdddb9f13a65f691f3dee
May Qiu <90876910+Mq0509@users.noreply.github.com>
2021-10-29 23:40:23 -0400

    Update helloName.java

--- cb/string-1/helloName.java
+++ cb/string-1/helloName.java
@@ -1,6 +1,6 @@
 /*Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".*/
 public class helloName{
-  public String helloName(String name) {
+  public static String helloName(String name) {
   return "Hello " + name + "!";
 }
   public static void main(String [] args){
