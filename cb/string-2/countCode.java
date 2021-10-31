8f04e7bb1f2118ba350f6cd922c4b9f54219c4fd
May Qiu <90876910+Mq0509@users.noreply.github.com>
2021-10-30 21:26:02 -0400

    Update countCode.java

---
+++ cb/string-2/countCode.java
@@ -0,0 +1,18 @@
+/* Return the number of times that the string "code" appears anywhere in the given string, except we'll accept any letter for the 'd', so "cope" and "cooe" count.*/
+
+public class countCode{
+public static int countCode(String str) {
+   int ans=0;
+   for (int i=0;i < str.length()-3;i ++){
+     if (str.substring(i,i+2).equals("co") && str.substring(i+3,i+4).equals("e")){
+       ans ++;
+      }
+    }
+     return ans;
+}
+public static void main(String [] args){
+  System.out.println(countCode("aaacodebbb")); /* → 1 */
+  System.out.println(countCode("codexxcode")); /* → 2 */
+  System.out.println(countCode("cozexxcope")); /* → 2 */
+}
+
