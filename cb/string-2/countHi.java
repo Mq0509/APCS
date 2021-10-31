f088fff0fa60e368b0ae6e6b2aacb9bf8cb498d0
May Qiu <90876910+Mq0509@users.noreply.github.com>
2021-10-30 21:07:09 -0400

    Update countHi.java

--- cb/string-2/countHi.java
+++ cb/string-2/countHi.java
@@ -1,17 +1,17 @@
 /* Return the number of times that the string "hi" appears anywhere in the given string. */
 public class countHi{
-public static int countHi(String str) {
-  int ans=0;
-  for (int i=0;i < str.length()-1;i ++){
-    if (str.substring(i,i+2).equals("hi")){
-      ans ++;
+  public static int countHi(String str) {
+   int ans=0;
+   for (int i=0;i < str.length()-1;i ++){
+     if (str.substring(i,i+2).equals("hi")){
+       ans ++;
+      }
     }
+     return ans;
   }
-  return ans;
-}
-public static void main(String [] args){
-  System.out.println (countHi("abc hi ho")); /* → 1*/
-  System.out.println (countHi("ABChi hi")); /*→ 2*/
-  System.out.println (countHi("hihi")); /*2*/
-}
+   public static void main(String [] args){
+    System.out.println (countHi("abc hi ho")); /* → 1*/
+    System.out.println (countHi("ABChi hi")); /*→ 2*/
+    System.out.println (countHi("hihi")); /*2*/
+  }
 }
