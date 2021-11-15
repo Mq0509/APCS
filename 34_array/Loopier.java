c293fd840a1b76598a67efca0dad294809f060b8
mqiu30 <mqiu30@stuy.edu>
2021-11-14 22:22:25 -0500

    init

---
+++ 34_array/Loopier.java
@@ -0,0 +1,9 @@
+public class Loopier{
+	public static void populateArray(){
+		int []x={Math.random()*5};
+		}
+	public static void main(String [] args){
+		int []x=new int [5];
+		System.out.println(populateArray(x));
+	}
+}
