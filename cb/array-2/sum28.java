6eb4596e709c9e475c58d07a2e3275a1bc070c29
May Qiu <90876910+Mq0509@users.noreply.github.com>
2021-11-16 20:54:25 -0500

    Update sum28.java

--- cb/array-2/sum28.java
+++ cb/array-2/sum28.java
@@ -1,3 +1,13 @@
+/*
+
+Given an array of ints, return true if the sum of all the 2's in the array is exactly 8.
+
+
+sum28([2, 3, 2, 2, 4, 2]) → true
+sum28([2, 3, 2, 2, 4, 2, 2]) → false
+sum28([1, 2, 3, 4]) → false
+*/
+
 public class sum28{
   public static boolean sum28(int[] nums) {
     int twos=0;
