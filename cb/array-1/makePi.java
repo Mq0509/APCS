ddfcdcccd4c3266d88cbf0eba460fd6c69206672
May Qiu <90876910+Mq0509@users.noreply.github.com>
2021-11-16 20:57:09 -0500

    Update makePi.java

---
+++ cb/array-1/makePi.java
@@ -0,0 +1,16 @@
+/*Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+makePi() → [3, 1, 4]
+*/
+
+import java.util.Arrays;
+
+public class makePi {
+	
+	public static int[] makePi() {
+		int[] pi = {3, 1, 4};
+		return pi;
+	}
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(makePi()));
+	}
+}
