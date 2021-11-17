4305a975c4edb0de08290a004550c6899cd73dce
May Qiu <90876910+Mq0509@users.noreply.github.com>
2021-11-16 20:53:48 -0500

    Update sum28.java

---
+++ cb/array-2/sum28.java
@@ -0,0 +1,18 @@
+public class sum28{
+  public static boolean sum28(int[] nums) {
+    int twos=0;
+    for (int i=0;i<nums.length; i++){
+      if(nums[i]==2){
+        twos++;
+      }
+    }
+      if (twos==4){
+        return true;}
+        else {return false;}
+    }
+public static void main(String [] args){
+  System.out.println(sum28(new int []{2,3,2,2,4,2}));
+  System.out.println(sum28(new int []{2,3,2,2,4,2,2}));
+  System.out.println(sum28(new int []{1,2,3,4}));
+}
+}
