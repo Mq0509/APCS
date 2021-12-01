49127fe8f5e254ff7c5118d58b362310824fa3f8
May Qiu <90876910+Mq0509@users.noreply.github.com>
2021-11-30 21:51:31 -0500

    Update TwoDimArray.java

--- 40_arrxarr/TwoDimArray.java
+++ 40_arrxarr/TwoDimArray.java
@@ -4,10 +4,18 @@
  * practice working with 2D arrays
  ******************************/
 
-// Clyde "Thluffy" Sinclair
-// APCS pd0
-// HW40 -- 2D arrays
+// Kleemex -- Lindsay Phung, May Qiu, Xinqing Lin
+// APCS pd7
+// HW40 -- Demand A Raise / 2D arrays
 // 2021-11-30
+// time spent: 0.5 hrs
+//
+// DISCO
+// - We can use helper functions to simplify sum2.
+//
+// QCC
+// - For loops are helpful for when you need to reference the index.
+// - What are the benefits to using helper functions? Perhaps they make the code more readable?
 
 public class TwoDimArray
 {
@@ -15,14 +23,14 @@ public class TwoDimArray
   //          uses a FOR loop
   public static void print1( int[][] a )
   {
-    for(int i=0;i<a.length;i++){
-      for (int i2 = 0; i2 < a[i].length; i2++) {
-        System.out.print(a[i][i2]);
-        System.out.print(",");
-        System.out.print(a[])
-      }
-      System.out.print(a[i][a.length-1]);
-      System.out.println();
+    for (int i = 0; i < a.length; i++) {
+        System.out.print("[");
+        for (int i2 = 0; i2 < a[i].length - 1; i2++) {
+            System.out.print(a[i][i2] + ",");
+        }
+        System.out.print(a[i][a[i].length - 1]);
+        System.out.print("]");
+        System.out.println();
     }
   }
 
@@ -31,15 +39,27 @@ public class TwoDimArray
   //          uses a FOREACH loop
   public static void print2( int[][] a )
   {
-    // YOUR IMPLEMENTATION HERE
+    for (int[] arr : a) {
+        System.out.print("[");
+        for (int value : arr) {
+            System.out.print(value + ",");
+        }
+        System.out.print("]");
+        System.out.println();
+    }
   }
 
 
   //postcond: returns sum of all items in 2D int array a
   public static int sum1( int[][] a )
   {
-    // YOUR IMPLEMENTATION HERE
-    return 0;
+    int summer = 0;
+    for (int[] arr : a) {
+      for (int value : arr) {
+        summer += value;
+      }
+    }
+    return summer;
   }
 
 
@@ -47,8 +67,11 @@ public class TwoDimArray
   //          * uses helper fxn sumRow
   public static int sum2( int [][] m )
   {
-    // YOUR IMPLEMENTATION HERE
-    return 0;
+    int summer = 0;
+    for (int i = 0; i < m.length; i++) {
+      summer += sumRow(i, m);
+    }
+    return summer;
   }
 
 
@@ -56,8 +79,11 @@ public class TwoDimArray
   //          uses a FOR loop
   public static int sumRow( int r, int[][] a )
   {
-        // YOUR IMPLEMENTATION HERE
-        return 0;
+    int summer = 0;
+    for (int i = 0; i < a[r].length; i++) {
+      summer += a[r][i];
+    }
+    return summer;
   }
 
 
@@ -67,6 +93,9 @@ public class TwoDimArray
   {
     // *** YOUR IMPLEMENTATION HERE ***
     int summer = 0;
+    for (int value : m[r]) {
+      summer += value;
+    }
     return summer;
   }
 
@@ -80,7 +109,6 @@ public class TwoDimArray
       print1(m1);
       print1(m2);
       print1(m3);
-          /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
       print2(m1);
       print2(m2);
       print2(m3);
@@ -92,9 +120,19 @@ public class TwoDimArray
       System.out.println("sum m1 : " + sum2(m1));
       System.out.println("sum m2 : " + sum2(m2));
       System.out.println("sum m3 : " + sum2(m3));
+      /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-
-    // your own custom test cases welcomed and encouraged
+      // your own custom test cases welcomed and encouraged
+      int [][] m4 = { {-3, -10, 5}, {4, -9, 5} };
+      int [][] m5 = { {0, 0}, {0, 0, 0}, {0, 0, 0, 0} };
+      System.out.print("custom test cases...\n");
+      System.out.print("testing sum1...\n");
+      System.out.println("sum m4 : " + sum1(m4));
+      System.out.println("sum m5 : " + sum1(m5));
+      System.out.print("testing sum2...\n");
+      System.out.println("sum m4 : " + sum2(m4));
+      System.out.println("sum m5 : " + sum2(m5));
   }
 
 }//end class TwoDimArray
+
