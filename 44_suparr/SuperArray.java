4fff38deab84847748fe581e94699bcdd3e4144c
mqiu30@stuy.edu <mqiu30@stuy.edu>
2021-12-08 17:49:03 -0500

    sjjs

---
+++ 44_suparr/SuperArray.java
@@ -0,0 +1,195 @@
+// Kleemex -- Lindsay Phung, May Qiu, Xinqing Lin
+// APCS1 pd7
+// HW44 -- Array of Grade 316 / expanding SuperArray functionality, encapsulation
+// 2021-12-07
+//
+// DISCO
+// - _size of an array is the number of meaningful values, not the number of available spaces.
+//
+// QCC
+// - Why do we write expand() instead of _data.expand() in add-at-index?
+// Idea: The dot operator is only used to reference a method or an instance variable from a class or an instance of a class. Also, expand() directly changes _data.
+
+/***************************
+ * class SuperArray version 2.0
+ * (SKELETON)
+ * Wrapper class for array. Facilitates
+ * resizing
+ * expansion
+ * read/write capability on elements
+ * adding an element to end of array
+ * adding an element at specified index
+ * removing an element at specified index
+ ***************************/
+
+public class SuperArray
+{
+
+  private int[] _data;  //underlying ("encapsulated") container
+  private int _size;    //number of elements in this SuperArray
+
+
+  //default constructor – initializes 10-item array
+  public SuperArray()
+  {
+    _data = new int[10];
+    _size = 0;
+  }
+
+
+  //output SuperArray in [a,b,c] format
+  public String toString()
+  {
+    String foo = "[";
+    for( int i = 0; i < _size; i++ ) {
+      foo += _data[i] + ",";
+    }
+    if ( foo.length() > 1 )
+      //shave off trailing comma
+      foo = foo.substring( 0, foo.length()-1 );
+    foo += "]";
+    return foo;
+  }
+
+
+  //double capacity of SuperArray
+  private void expand()
+  {
+    int[] temp = new int[ _data.length * 2 ];
+    for( int i = 0; i < _data.length; i++ )
+      temp[i] = _data[i];
+    _data = temp;
+  }
+
+
+  //accessor -- return value at specified index
+  public int get( int index )
+  {
+    return _data[index];
+  }
+
+
+  //mutator -- set value at index to newVal,
+  //           return old value at index
+  public int set( int index, int newVal )
+  {
+    int temp = _data[index];
+    _data[index] = newVal;
+    _size++;
+    return temp;
+  }
+
+
+  //adds an item after the last item
+  public void add( int newVal )
+  {
+    if (_size + 1 > _data.length) {
+      expand();
+    }
+    _data[_size] = newVal;
+    _size++;
+  }
+
+
+  //inserts an item at index
+  public void add( int index, int newVal )
+  {
+    if (_size + 1 > _data.length) {
+      expand();
+    }
+    _size++;
+    for (int i = _size; i > index; i--){ // moves values to the right, starting with the rightmost value
+      _data[i] = _data[i - 1];
+    }
+    _data[index] = newVal;
+  }
+
+
+  //removes the item at index
+  //shifts elements left to fill in newly-empted slot
+  public void remove( int index )
+  {
+    for (int i = index; i < _size - 1; i++) { // moves values to the left, starting with the leftmost value
+      _data[i] = _data[i + 1];
+    }
+    _data[_size] = 0;
+    _size--;
+  }
+
+
+  //return number of meaningful items in _data
+  public int size()
+  {
+    return _size;
+  }
+
+
+
+  //main method for testing
+  public static void main( String[] args )
+  {
+      SuperArray curtis = new SuperArray();
+      System.out.print( "---------------- \n");
+      System.out.println( "Printing empty SuperArray curtis..." );
+      System.out.println( curtis );
+
+      for( int i = 0; i < curtis._data.length; i++ ) {
+      curtis.set( i, i * 2 );
+      }
+      System.out.print( "---------------- \n");
+      System.out.println("Printing populated SuperArray curtis...");
+      System.out.println(curtis);
+      for( int i = 0; i < 3; i++ ) {
+      curtis.expand();
+      System.out.println("Printing expanded SuperArray curtis...");
+      System.out.println(curtis);
+      System.out.println("new length of underlying array: "
+      + curtis._data.length );
+    }
+
+    SuperArray mayfield = new SuperArray();
+    System.out.print( "---------------- \n");
+    System.out.println("Printing empty SuperArray mayfield...");
+    System.out.println(mayfield);
+
+    mayfield.add(5);
+    mayfield.add(4);
+    mayfield.add(3);
+    mayfield.add(2);
+    mayfield.add(1);
+
+    System.out.print( "---------------- \n");
+    System.out.println("Printing populated SuperArray mayfield...");
+    System.out.println(mayfield);
+
+    System.out.print( "---------------- \n");
+    mayfield.remove(3);
+    System.out.println("Printing SuperArray mayfield post-remove...");
+    System.out.println(mayfield);
+    System.out.print( "---------------- \n");
+    mayfield.remove(3);
+    System.out.println("Printing SuperArray mayfield post-remove...");
+    System.out.println(mayfield);
+
+    System.out.print( "---------------- \n");
+    mayfield.add(3,99);
+    System.out.println("Printing SuperArray mayfield post-insert...");
+    System.out.println(mayfield);
+
+    System.out.print( "---------------- \n");
+    mayfield.add(2,88);
+    System.out.println("Printing SuperArray mayfield post-insert...");
+    System.out.println(mayfield);
+    System.out.println("Printing SuperArray mayfield post-insert...");
+    System.out.print( "---------------- \n");
+    mayfield.add(1,77);
+    System.out.println("Printing SuperArray mayfield post-insert...");
+    System.out.println(mayfield);
+    System.out.println(mayfield.size());
+    System.out.print( "---------------- \n");
+      /*~~~~~~~~move~me~down~~~~~~~~~~~~~~V~~~~~~~~
+      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|~~~~~~~~*/
+  }//end main()
+
+
+}//end class
