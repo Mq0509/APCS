0f24eca2b9dfcb96374dbe946b805f79bb2ef194
May Qiu <90876910+Mq0509@users.noreply.github.com>
2022-01-04 22:25:22 -0500

    Update SelectionSort.java

---
+++ 52_selectionsort/SelectionSort.java
@@ -0,0 +1,157 @@
+// Grey Leche: Faiyaz Rafee, May Qiu, David Deng
+// APCS pd7
+// HW52 -- Selection, Natch / implementing selection sort
+// 2022-01-05w
+// time spent: 0.5 hrs
+
+/******************************
+ *   class SelectionSort -- implements SelectionSort algorithm
+ *
+ * ALGO:
+ *
+ * DISCO
+ *
+ * QCC
+ * q0: How many passes to sort n elements?
+ * a0: n - 1 passes
+ * q1: What do you know after pass p?
+ * a1: p elements are confirmed to be in their final sorted location
+ * q2: How do you know if sorted?
+ * a2: When `# of elements - 1` passes are reached
+ * q3: What does a pass boil down to?
+ * a3: A pass boils down to:
+ * - traversing the array from the beginning to index pass - 1
+ * - finding the index at which the maximum value is present
+ * - swapping the value at this maximum value index with the value at index pass
+ * - incrementing pass so we ignore the confirmed index on our next traversal
+ ******************************/
+
+
+import java.util.ArrayList;
+
+public class SelectionSort
+{
+
+  //~~~~~~~~~~~~~~~~~~~ HELPER METHODS ~~~~~~~~~~~~~~~~~~~
+  //precond: lo < hi && size > 0
+  //postcond: returns an ArrayList of random integers
+  //          from lo to hi, inclusive
+  public static ArrayList populate( int size, int lo, int hi )
+  {
+    ArrayList<Integer> retAL = new ArrayList<Integer>();
+    while( size > 0 ) {
+      //     offset + rand int on interval [lo,hi]
+      retAL.add( lo + (int)( (hi-lo+1) * Math.random() ) );
+      size--;
+    }
+    return retAL;
+  }
+
+  //randomly rearrange elements of an ArrayList
+  public static void shuffle( ArrayList al )
+  {
+    int randomIndex;
+    for( int i = al.size()-1; i > 0; i-- ) {
+      //pick an index at random
+      randomIndex = (int)( (i+1) * Math.random() );
+      //swap the values at position i and randomIndex
+      al.set( i, al.set( randomIndex, al.get(i) ) );
+    }
+  }
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+  // VOID version of SelectionSort
+  // Rearranges elements of input ArrayList
+  // postcondition: data's elements sorted in ascending order
+  public static void selectionSortV( ArrayList<Comparable> data )
+  {
+    //note: this version places greatest value at "rightmost" end
+
+    //maxPos will point to position of SELECTION (greatest value)
+    int maxPos;
+
+    for(int pass = data.size() - 1; pass > 0; pass--) {
+      System.out.println( "\nbegin pass " + (data.size()-pass) );//diag
+      maxPos = pass;
+
+      for(int i = 1; i < pass; i++) {
+        if (data.get(i).compareTo(data.get(maxPos)) > 0) {
+          maxPos = i;
+        }
+        System.out.println( "maxPos: " + maxPos );//diag
+        System.out.println( data );//diag
+      }
+      Comparable dummy = data.get(pass);
+      data.set(pass, data.get(maxPos));
+      data.set(maxPos, dummy);
+
+      System.out.println( "after swap: " +  data );//diag
+    }
+  }//end selectionSort
+
+
+  // ArrayList-returning selectionSort
+  // postcondition: order of input ArrayList's elements unchanged
+  //                Returns sorted copy of input ArrayList.
+  public static ArrayList<Comparable> selectionSort( ArrayList<Comparable> input )
+  {
+    //declare and initialize empty ArrayList for copying
+    ArrayList<Comparable> data = new ArrayList<Comparable>();
+
+    //copy input ArrayList into working ArrayList
+    for( Comparable o : input )
+      data.add( o );
+
+    //sort working ArrayList
+    selectionSortV( data );
+
+    return data;
+  }//end selectionSort
+
+
+  public static void main( String [] args )
+  {
+
+    
+    ArrayList glen = new ArrayList<Integer>();
+    glen.add(7);
+    glen.add(1);
+    glen.add(5);
+    glen.add(12);
+    glen.add(3);
+    System.out.println( "ArrayList glen before sorting:\n" + glen );
+    selectionSortV(glen);
+    System.out.println( "ArrayList glen after sorting:\n" + glen );
+
+    ArrayList coco = populate( 10, 1, 1000 );
+    System.out.println( "ArrayList coco before sorting:\n" + coco );
+    selectionSortV(coco);
+    System.out.println( "ArrayList coco after sorting:\n" + coco );
+    
+
+    /*==========for AL-returning methods==========
+      ArrayList glen = new ArrayList<Integer>();
+      glen.add(7);
+      glen.add(1);
+      glen.add(5);
+      glen.add(12);
+      glen.add(3);
+      System.out.println( "ArrayList glen before sorting:\n" + glen );
+      ArrayList glenSorted = selectionSort( glen );
+      System.out.println( "sorted version of ArrayList glen:\n"
+      + glenSorted );
+      System.out.println( "ArrayList glen after sorting:\n" + glen );
+
+      ArrayList coco = populate( 10, 1, 1000 );
+      System.out.println( "ArrayList coco before sorting:\n" + coco );
+      ArrayList cocoSorted = selectionSort( coco );
+      System.out.println( "sorted version of ArrayList coco:\n"
+      + cocoSorted );
+      System.out.println( "ArrayList coco after sorting:\n" + coco );
+      System.out.println( coco );
+      ============================================*/
+
+  }//end main
+
+}//end class SelectionSort
