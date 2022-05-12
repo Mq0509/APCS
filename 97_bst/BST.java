7139c3924017207cce006ddf08a48c84a52df226
May Qiu <90876910+Mq0509@users.noreply.github.com>
2022-05-12 07:45:33 -0400

    Create BST.java

--- 97_bst/BST.java
+++ 97_bst/BST.java
@@ -1,30 +1,13 @@
 /**
  * class BST
- * v2:partial
- * SKELETON
- * Implementation of the BINARY SEARCH TREE abstract data type (ADT)
- *
- * A BST maintains the invariant that, for any node N with value V,
- * L<V && V<R, where L and R are node values in N's left and right
- * subtrees, respectively.
- * (Any value in a node's left subtree must be less than its value,
- *  and any value in its right subtree must be greater.)
- *
- * This BST implementation only holds ints (its nodes have int cargo)
- */
-
-public class BST
-{
-
-  //instance variables / attributes of a BST:
-  TreeNode _root;
-
-  /**
-   * default constructor
+ * v2:tor
    */
   BST()
   {
-    _root = n
+    _root = null;
+  }
+
+
   /**
    * void insert( int )
    * Adds a new data element to tree.
@@ -35,18 +18,180 @@ public class BST
 
     if ( _root == null ) {
       _root = newNode;
-     
+      return;
+    }
+    insert( _root, newNode );
+  }
+  //recursive helper for insert(int)
+  public void insert( TreeNode stRoot, TreeNode newNode )
+  {
+    if ( newNode.getValue() < stRoot.getValue() ) {
+      //if no left child, make newNode the left child
+      if ( stRoot.getLeft() == null )
+        stRoot.setLeft( newNode );
+      else //recurse down left subtree
+        insert( stRoot.getLeft(), newNode );
+      return;
+    }
+    else { // new val >= curr, so look down right subtree
+      //if no right child, make newNode the right child
+      if ( stRoot.getRight() == null )
+        stRoot.setRight( newNode );
+      else //recurse down right subtree
+        insert( stRoot.getRight(), newNode );
+      return;
     }
   }//end insert()
 
 
-  //~~() );
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  //~~~~~~~~~~~~~v~~TRAVERSALS~~v~~~~~~~~~~~~~~~~~~~~~
+  // each traversal should simply print to standard out
+  // the nodes visited, in order
+
+  //process root, recurse left, recurse right
+  public void preOrderTrav()
+  {
+    preOrderTrav( _root );
+  }
+  public void preOrderTrav( TreeNode currNode )
+  {
+    if ( currNode == null )
+      return;
+    System.out.print( currNode.getValue() + " " );
+    preOrderTrav( currNode.getLeft() );
+    preOrderTrav( currNode.getRight() );
+  }
+
+  //recurse left, process root, recurse right
+  public void inOrderTrav()
+  {
+    inOrderTrav( _root );
+  }
+  public void inOrderTrav( TreeNode currNode )
+  {
+    if ( currNode == null )
+      return;
+    inOrderTrav( currNode.getLeft() );
     System.out.print( currNode.getValue() + " " );
     inOrderTrav( currNode.getRight() );
   }
 
   //recurse left, recurse right, process root
-  public void
+  public void postOrderTrav()
+  {
+    postOrderTrav( _root );
+  }
+  public void postOrderTrav( TreeNode currNode )
+  {
+    if ( currNode == null )
+      return;
+    postOrderTrav( currNode.getLeft() );
+    postOrderTrav( currNode.getRight() );
+    System.out.print( currNode.getValue() + " "  );
+  }
+
+  public String inOrderTravStr( TreeNode currNode ) {
+    String retStr = "";
+    if ( currNode == null )
+      return retStr;
+    retStr += inOrderTravStr( currNode.getLeft() );
+    retStr += " " + currNode.getValue();
+    retStr += inOrderTravStr( currNode.getRight() );
+    return retStr;
+  }
+  public String preOrderTravStr( TreeNode currNode ) {
+    String retStr = "";
+    if ( currNode == null )
+      return retStr;
+    retStr += " " + currNode.getValue();
+    retStr += preOrderTravStr( currNode.getLeft() );
+    retStr += preOrderTravStr( currNode.getRight() );
+    return retStr;
+  }
+  public String postOrderTravStr( TreeNode currNode ) {
+    String retStr = "";
+    if ( currNode == null )
+      return retStr;
+    retStr += postOrderTravStr( currNode.getLeft() );
+    retStr += postOrderTravStr( currNode.getRight() );
+    retStr += " " + currNode.getValue();
+    return retStr;
+  }
+
+  //~~~~~~~~~~~~~^~~TRAVERSALS~~^~~~~~~~~~~~~~~~~~~~~~
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+  //~~~~~~~~~~~~~v~~MISC.HELPERS~~v~~~~~~~~~~~~~~~~~~~
+  public boolean isLeaf( TreeNode node )
+  {
+    return ( node.getLeft() == null
+             &&
+             node.getRight() == null );
+  }
+  //~~~~~~~~~~~~~^~~MISC.HELPERS~~^~~~~~~~~~~~~~~~~~~~
+
+
+  //overridden toString
+  public String toString()
+  {
+    return
+      " pre-order trav:" + preOrderTravStr( _root ) + '\n' +
+      "  in-order trav:" + inOrderTravStr( _root ) + '\n' +
+      "post-order trav:" + postOrderTravStr( _root ) + '\n' +
+      "         height: " + height() + '\n' +
+      "     num leaves: " + numLeaves() 
+      ;
+  }
+
+
+  /**
+   * TreeNode search(int)
+   * returns pointer to node containing target,
+   * or null if target not found
+   */
+  TreeNode search( int target )
+  {
+    return search( target, _root );
+  }
+  TreeNode search( int target, TreeNode currNode )
+  {
+    if ( currNode==null || currNode.getValue()==target )
+      return currNode;
+    else if ( target < currNode.getValue() )
+      return search( target, currNode.getLeft() );
+    else if ( target > currNode.getValue() )
+      return search( target, currNode.getRight() );
+    else
+      return null; //to get past compiler
+  }
+
+
+  /**
+   * int height()
+   * returns height of this tree (length of longest leaf-to-root path)
+   * eg: a 1-node tree has height 0
+   */
+  public int height()
+  {
+    return height( _root );
+  }
+  //recursive helper for height()
+  public int height( TreeNode currNode )
+  {
+    if ( currNode==null ) //Q: Why cannot use .equals() ?
+      return 0;
+    if ( isLeaf(currNode) )
+      return 0;
+    else //height is 1 for this node + height of deepest subtree
+      return 1 + Math.max( height(currNode.getLeft()),
+                           height(currNode.getRight()) );
+  }
+
+
+  /**
+   * int numLeaves()
    * returns number of leaves in tree
    */
   public int numLeaves()
